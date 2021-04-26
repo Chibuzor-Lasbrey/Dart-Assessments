fbb44054e4026dc3ad4100cf37cb158d762d9d5e
Chibuzor-Lasbrey <79379832+Chibuzor-Lasbrey@users.noreply.github.com>
2021-04-26 19:11:48 +0100

    Create lab3.dart

---
+++ lab3.dart
@@ -0,0 +1,20 @@
+// A dart program that uses listed data for student data to calculate CGPA and CGPA percentage using set formulas
+PA 
+import 'dart:io';
+//declaration of variables
+var grade = [90, 80, 70, 80, 90];
+int n = 5;
+var total=0;
+
+//app start point
+void main(){
+int sum = 0;
+//summation of list
+for (int e in grade) {
+  sum += e;}
+//formular for cgpa
+double cgpa = sum/n;
+double percentage = cgpa * 9.5;
+print("cgpa is: $cgpa");
+print('percentage is: $percentage');
+}
